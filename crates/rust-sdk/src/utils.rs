de57f6eea23a12d05cdec8a7a424cebfe12eb293
zees-dev <dev.786zshan@gmail.com>
2023-09-06 14:51:41 +1200

    added logging macro to sdk - to call host

---
+++ crates/rust-sdk/src/utils.rs
@@ -0,0 +1,22 @@
+
+fn log(msg: &str) {
+  let msg_bytes = msg.as_bytes();
+  let ptr = msg_bytes.as_ptr() as u32;
+  let len = msg_bytes.len() as u32;
+  unsafe {
+      crate::host_log(ptr, len);
+  }
+}
+
+#[macro_export]
+macro_rules! log {
+  ($($t:tt)*) => {
+      let msg = &format_args!($($t)*).to_string();
+      let msg_bytes = msg.as_bytes();
+      let ptr = msg_bytes.as_ptr() as u32;
+      let len = msg_bytes.len() as u32;
+      unsafe {
+          host_log(ptr, len);
+      }
+  }
+}
