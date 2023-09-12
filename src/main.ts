df97c0722e6fad0c6a1e62feb2616aff65fd0606
zees-dev <dev.786zshan@gmail.com>
2023-09-13 01:54:27 +1200

    working wasi example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,60 @@
 // load host wasm module (isomorphic bls runtime/extensions)
 import init, { Blockless, BlocklessConfig } from "../bls-runtime-wasm/pkg";
+import WASI from "wasi-js";
+import browserBindings from "wasi-js/dist/bindings/browser";
+
+import { fs } from 'memfs';
+(fs as any).writeSync = (fd, buffer, offset, length, position, callback) => {
+    var string = new TextDecoder().decode(buffer);
+
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(string, "text/html");
+
+    // Get the <head> element from the parsed document
+    const parsedHead = htmlDoc.head;
+
+    // Get the main document's <head> element
+    const mainDocumentHead = document.head;
+
+    // Move all children from parsedHead to mainDocumentHead
+    while (parsedHead.firstChild) {
+        mainDocumentHead.appendChild(parsedHead.firstChild);
+    }
+
+    // Get the <body> element from the parsed document
+    const parsedBody = htmlDoc.body;
+
+    // Get the main document's <body> element
+    const mainDocumentBody = document.body;
+
+    // Move all children from parsedBody to mainDocumentBody
+    while (parsedBody.firstChild) {
+        mainDocumentBody.appendChild(parsedBody.firstChild);
+    }
+
+    if (callback) {
+        callback(null, length);
+    }
+};
+
+const wasi = new WASI({
+    args: [],
+    env: {
+        BLS_REQUEST_METHOD: "GET",
+        BLS_REQUEST_PATH: "/",
+        BLS_REQUEST_QUERY: "",
+    },
+    bindings: { ...browserBindings, fs },
+});
+
 await init();
 
 const config: BlocklessConfig = {
-    env: {},
+    env: {
+        BLS_REQUEST_METHOD: "GET",
+        BLS_REQUEST_PATH: "/",
+        BLS_REQUEST_QUERY: "",
+    },
     args: [],
     // "fs_root_path": "/", 
     // "drivers_root_path": "/drivers", 
@@ -20,10 +71,12 @@ const config: BlocklessConfig = {
 };
 const bls = new Blockless(config);
 
-const wasmPath = "../target/wasm32-unknown-unknown/release/rust_sdk.wasm";
+const wasmPath = "../release.wasm";
+// const wasmPath = "../target/wasm32-unknown-unknown/release/rust_sdk.wasm";
 // const { instance, module } = await WebAssembly.instantiateStreaming(fetch(wasmPath), {});
-const module = await WebAssembly.compileStreaming(fetch(wasmPath));
-bls.instantiate(module, {
+const wasmModule = await WebAssembly.compileStreaming(fetch(wasmPath));
+bls.instantiate(wasmModule, {
+    wasi_snapshot_preview1: wasi.wasiImport,
     browser: {
         run_reqwest: (ptr: number, len: number) => {
             const instance = (globalThis as any).instance; // global variable value set by bls.instantiate
@@ -34,9 +87,13 @@ bls.instantiate(module, {
         },
     },
 });
+// const exitCode = bls.start();
+// console.log("Exit code: " + exitCode);
 
-const exitCode = bls.start();
-console.log("Exit code: " + exitCode);
+const wasmInstance = await WebAssembly.instantiate(wasmModule, { wasi_snapshot_preview1: wasi.wasiImport });
+wasi.start(wasmInstance);
+
+console.log("running!");
 
 // // specify imports for the guest wasm module
 // const guestImports = {
@@ -60,61 +117,61 @@ console.log("Exit code: " + exitCode);
 
 // Invoke the `array_sum` exported method and
 // log the result to the console
-function arraySum(array: Uint8Array, instance: WebAssembly.Instance) {
-    // copy the contents of `array` into the
-    // module's memory and get the offset
-    const ptr = copyMemory(instance, array);
-    // invoke the module's `array_sum` exported function
-    // and log the result
-    const res = (instance.exports as any).array_sum(ptr, array.length);
-    console.log("Result: " + res);
-}
+// function arraySum(array: Uint8Array, instance: WebAssembly.Instance) {
+//     // copy the contents of `array` into the
+//     // module's memory and get the offset
+//     const ptr = copyMemory(instance, array);
+//     // invoke the module's `array_sum` exported function
+//     // and log the result
+//     const res = (instance.exports as any).array_sum(ptr, array.length);
+//     console.log("Result: " + res);
+// }
 
-function upper(input: string, instance: WebAssembly.Instance) {
-    // transform the input string into its UTF-8
-    // representation
-    const bytes = new TextEncoder().encode(input);
-    // copy the contents of the string into
-    // the module's memory
-    const ptr = copyMemory(instance, bytes);
-    // call the module's `upper` function and
-    // get the offset into the memory where the
-    // module wrote the result string
-    const res_ptr = (instance.exports as any).upper(ptr, bytes.length);
-    // read the string from the module's memory,
-    // store it, and log it to the console
-    const result = readString(res_ptr, bytes.length, instance);
-    console.log(result);
-    // the JavaScript runtime took ownership of the
-    // data returned by the module, which did not
-    // deallocate it - so we need to clean it up
-    deallocGuestMemory(res_ptr, bytes.length, instance);
-}
+// function upper(input: string, instance: WebAssembly.Instance) {
+//     // transform the input string into its UTF-8
+//     // representation
+//     const bytes = new TextEncoder().encode(input);
+//     // copy the contents of the string into
+//     // the module's memory
+//     const ptr = copyMemory(instance, bytes);
+//     // call the module's `upper` function and
+//     // get the offset into the memory where the
+//     // module wrote the result string
+//     const res_ptr = (instance.exports as any).upper(ptr, bytes.length);
+//     // read the string from the module's memory,
+//     // store it, and log it to the console
+//     const result = readString(res_ptr, bytes.length, instance);
+//     console.log(result);
+//     // the JavaScript runtime took ownership of the
+//     // data returned by the module, which did not
+//     // deallocate it - so we need to clean it up
+//     deallocGuestMemory(res_ptr, bytes.length, instance);
+// }
 
 // Read a string from the instance's memory.
-function readString(ptr: number, len: number, instance: WebAssembly.Instance) {
-    const m = new Uint8Array((instance.exports.memory as any).buffer, ptr, len);
-    const decoder = new TextDecoder("utf-8");
-    // return a slice of size `len` from the module's
-    // memory, starting at offset `ptr`
-    return decoder.decode(m.slice(0, len));
-}
+// function readString(ptr: number, len: number, instance: WebAssembly.Instance) {
+//     const m = new Uint8Array((instance.exports.memory as any).buffer, ptr, len);
+//     const decoder = new TextDecoder("utf-8");
+//     // return a slice of size `len` from the module's
+//     // memory, starting at offset `ptr`
+//     return decoder.decode(m.slice(0, len));
+// }
 
-function deallocGuestMemory(ptr: number, len: number, instance: WebAssembly.Instance) {
-    // call the module's `dealloc` function
-    (instance.exports as any).dealloc(ptr, len);
-}
+// function deallocGuestMemory(ptr: number, len: number, instance: WebAssembly.Instance) {
+//     // call the module's `dealloc` function
+//     (instance.exports as any).dealloc(ptr, len);
+// }
 
-// Copy `data` into the `instance` exported memory buffer.
-function copyMemory(instance: WebAssembly.Instance, data: Uint8Array) {
-    // the `alloc` function returns an offset in
-    // the module's memory to the start of the block
-    const ptr = (instance.exports as any).alloc(data.length);
+// // Copy `data` into the `instance` exported memory buffer.
+// function copyMemory(instance: WebAssembly.Instance, data: Uint8Array) {
+//     // the `alloc` function returns an offset in
+//     // the module's memory to the start of the block
+//     const ptr = (instance.exports as any).alloc(data.length);
 
-    // create a typed `ArrayBuffer` at `ptr` of proper size
-    const mem = new Uint8Array((instance.exports.memory as any).buffer, ptr, data.length);
-    // copy the content of `data` into the memory buffer
-    mem.set(new Uint8Array(data));
-    // return the pointer
-    return ptr;
-}
+//     // create a typed `ArrayBuffer` at `ptr` of proper size
+//     const mem = new Uint8Array((instance.exports.memory as any).buffer, ptr, data.length);
+//     // copy the content of `data` into the memory buffer
+//     mem.set(new Uint8Array(data));
+//     // return the pointer
+//     return ptr;
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,36 @@
-// import { defineConfig } from 'vite';
-// import nodePolyfills from 'vite-plugin-node-stdlib-browser'
-
-// export default defineConfig({
-//   server: {
-//     port: 8080,
-//   },
-// 	build: {
-// 		target: 'esnext', // support top-level await
-// 	},
-//   plugins: [nodePolyfills()]
-// });
-
-
 import { defineConfig } from 'vite';
-// import wasmPack from 'vite-plugin-wasm-pack';
-// import wasm from "vite-plugin-wasm";
-// import topLevelAwait from "vite-plugin-top-level-await";
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 export default defineConfig({
   server: {
     port: 8080,
   },
-  build: {
-    target: 'esnext',
-  },
-  plugins: [
-    // wasm(),
-    // wasmPack('.'),
-    // topLevelAwait()
-  ],
+	build: {
+		target: 'esnext', // support top-level await
+	},
+  plugins: [nodePolyfills()]
 });
+
+
+// import { defineConfig } from 'vite';
+// import wasmPack from 'vite-plugin-wasm-pack';
+// import wasm from "vite-plugin-wasm";
+// import topLevelAwait from "vite-plugin-top-level-await";
+
+// export default defineConfig({
+//   server: {
+//     port: 8080,
+//   },
+//   build: {
+//     target: 'esnext',
+//   },
+//   plugins: [
+//     // wasm(),
+//     // wasmPack('.'),
+//     // topLevelAwait()
+//   ],
+//   define: {
+//     'process.env': process.env,
+//     // 'global': globalThis,
+//   }
+// });
