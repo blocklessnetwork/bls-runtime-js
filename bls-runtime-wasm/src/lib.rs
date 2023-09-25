20928d6f5e80064197188c22039958b10d3e9f0f
zees-dev <dev.786zshan@gmail.com>
2023-09-25 23:42:14 +1300

    cleanup

--- bls-runtime-wasm/src/lib.rs
+++ bls-runtime-wasm/src/lib.rs
@@ -1,15 +1,25 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use std::{sync::{Arc, Mutex}, cell::Ref};
+use std::cell::RefCell;
+use std::io::{Read, Write};
+
+pub mod fs;
+pub mod utils;
+
+use bls_common::{types::{ModuleCall, ModuleCallResponse}, http::HttpResponse};
+
 use serde::{Deserialize, Serialize};
 use js_sys::{Map, Object, Reflect, WebAssembly};
-use wasm_bindgen::{prelude::{wasm_bindgen, JsValue}, JsError};
-use wasm_bindgen::JsCast;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{JsValue, JsCast, JsError};
+use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen_futures::{JsFuture, spawn_local};
 use web_sys::{Request, RequestInit, RequestMode, Response, console};
-
-pub mod utils;
+use wasmer::{imports, Imports, Exports, ExternType, TypedFunction, Instance, Module, Store, Function, FunctionEnv, FunctionEnvMut, Memory, AsStoreRef, AsStoreMut, Value, MemoryView};
+use wasmer_wasi::Pipe;
+use wasmer::NativeWasmTypeInto;
+use wasmer_wasi::{WasiError, WasiFunctionEnv, WasiState};
 
 // https://github.com/rustwasm/console_error_panic_hook
 #[wasm_bindgen]
@@ -31,54 +41,8 @@ macro_rules! console_error {
     ($($t:tt)*) => (error(&format_args!($($t)*).to_string()))
 }
 
-// #[wasm_bindgen]
-// pub fn host_call(data: &[u8]) {
-//     let string = std::str::from_utf8(data).unwrap();
-//     console_log!("Host Log: {}", string);
-// }
-
-// #[wasm_bindgen]
-// pub async fn run_wasm(wasm_module: &[u8]) -> Result<(), JsValue> {
-//     init_panic_hook();
-//     console_log!("instantiating a new wasm module directly");
-
-//     let imports_obj = {
-//         let map = Map::new();
-//         let imports: JsValue = Imports.into();
-
-//         bind(&imports, "host_log")?;
-//         bind(&imports, "host_call")?;
-
-//         map.set(&JsValue::from("blockless"), &imports);
-//         Object::from_entries(&map.into())?
-//     };
-
-//     let buffer = JsFuture::from(WebAssembly::instantiate_buffer(wasm_module, &imports_obj)).await?;
-//     let instance: WebAssembly::Instance = Reflect::get(&buffer, &"instance".into())?.dyn_into()?;
-//     let exports = instance.exports();
-
-//     // TODO: FIX THIS - storing the instance globally
-//     unsafe {
-//         GUEST_INSTANCE = Some(instance);
-//     }
-
-//     let start_func = Reflect::get(&exports, &"start".into())?
-//         .dyn_into::<Function>()
-//         .map_err(|_| "not a function")?;
-
-//     start_func.call0(&JsValue::undefined())?;
-
-//     // console_log!("created module has {} pages of memory", mem.grow(0));
-//     // console_log!("giving the module 4 more pages of memory");
-//     // mem.grow(4);
-//     // console_log!("now the module has {} pages of memory", mem.grow(0));
-
-//     Ok(())
-// }
-
 // const WASM: &[u8] = include_bytes!("../../target/wasm32-unknown-unknown/release/rust_sdk.wasm");
 // const WASM: &[u8] = include_bytes!("../../simple.wasm");
-
 // #[wasm_bindgen(start)]
 // fn start() {
 //     wasm_bindgen_futures::spawn_local(async {
@@ -87,32 +51,6 @@ macro_rules! console_error {
 //             Err(e) => console_error!("{:?}", e),
 //         }
 //     });
-// }
-
-// {"page":1,"per_page":6,"total":12,"total_pages":2,"data":[{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},{"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},{"id":3,"name":"true red","year":2002,"color":"#BF1932","pantone_value":"19-1664"},{"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},{"id":5,"name":"tigerlily","year":2004,"color":"#E2583E","pantone_value":"17-1456"},{"id":6,"name":"blue turquoise","year":2005,"color":"#53B0AE","pantone_value":"15-5217"}],"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
-
-// #[wasm_bindgen]
-// pub async fn run_reqwest(url: &str) -> Result<JsValue, JsValue> {
-// pub async fn run_reqwest(url: &str) -> Result<Vec<u8>, &'static str> {
-//     let res = reqwest::Client::new()
-//         // .get("https://reqres.in/api/products")
-//         .get(url)
-//         .header("Accept", "application/vnd.github.v3+json")
-//         .send()
-//         .await
-//         .map_err(|_| "failed to send request")?;
-
-//     let bytes = res.bytes().await.map_err(|_| "failed to read response bytes")?;
-//     Ok(bytes.to_vec())
-
-//     // let products: bls_common::Products = serde_json::from_slice(&bytes).unwrap();
-//     // Ok(serde_wasm_bindgen::to_value(&products).unwrap())
-
-//     // let res_str = serde_json::to_string(&products).unwrap();
-//     // console_log!("run_reqwest: {}", res_str);
-
-//     // let result_bytes = serde_json::to_vec(&products).unwrap();
-//     // Ok(result_bytes)
 // }
 
 
@@ -144,16 +82,6 @@ extern "C" {
 //     virtual_fs::{Pipe, PipeTx, PipeRx, DuplexPipe},
 //     WasiError, WasiFunctionEnv, WasiEnvBuilder, runtime::{task_manager::WebThreadPool, web::WebRuntime}
 // };
-pub mod fs;
-use crate::fs::MemFS;
-
-use wasmer::{imports, Imports, Exports, ExternType, TypedFunction, Instance, Module, Store, Function, FunctionEnv, FunctionEnvMut, Memory, AsStoreRef, AsStoreMut, Value, MemoryView};
-use wasmer_wasi::Pipe;
-use wasmer::NativeWasmTypeInto;
-use wasmer_wasi::{WasiError, WasiFunctionEnv, WasiState};
-use std::{sync::{Arc, Mutex}, cell::Ref};
-use std::cell::RefCell;
-use std::io::{Read, Write};
 
 #[wasm_bindgen]
 pub struct Blockless {
@@ -251,9 +179,9 @@ impl Blockless {
         let fs = {
             let fs = js_sys::Reflect::get(&config, &"fs".into())?;
             if fs.is_undefined() {
-                MemFS::new()?
+                fs::MemFS::new()?
             } else {
-                MemFS::from_js(fs)?
+                fs::MemFS::from_js(fs)?
             }
         };
 
@@ -289,12 +217,12 @@ impl Blockless {
     }
 
     #[wasm_bindgen(getter)]
-    pub fn fs(&mut self) -> Result<MemFS, JsValue> {
+    pub fn fs(&mut self) -> Result<fs::MemFS, JsValue> {
         let state = self.wasi_env.data_mut(&mut self.store).state();
         let mem_fs = state
             .fs
             .fs_backing
-            .downcast_ref::<MemFS>()
+            .downcast_ref::<fs::MemFS>()
             .ok_or_else(|| js_sys::Error::new("Failed to downcast to MemFS"))?;
         Ok(mem_fs.clone())
     }
@@ -332,52 +260,6 @@ impl Blockless {
     }
 
     fn get_host_imports(&mut self) -> Result<Imports, JsValue> {
-        // let memory = self.instance.unwrap().exports().find_map(|export| {
-        //     match export.ty().to_owned() {
-        //         ExternType::Memory(memory) => Some(memory),
-        //         _ => None,
-        //     }
-        // });
-
-        // let log_fn = Closure::wrap(Box::new(move |ptr: u32, len: u32| {
-        //     let Some(ref instance) = (*instance_arc.borrow()).instance else {
-        //         console_error!("Guest instance should have been set");
-        //         return;
-        //     };
-        //     host_log(instance, ptr, len)
-        // }) as Box<dyn FnMut(u32, u32)>);
-
-        // let instance_arc = self.config.clone();
-        // let host_call_fn = Closure::wrap(Box::new(move |ptr: u32, len: u32| {
-        //     let Some(ref _instance) = (*instance_arc.borrow()).instance else {
-        //         console_error!("Guest instance should have been set");
-        //         return 0;
-        //     };
-
-        //     host_call(instance_arc.clone(), ptr, len)
-        // }) as Box<dyn FnMut(u32, u32) -> u32>);
-
-        // js_sys::Reflect::set(&blockless_exports, &JsValue::from("host_log"), log_fn.as_ref().unchecked_ref())?;
-        // log_fn.forget();
-
-        // js_sys::Reflect::set(&blockless_exports, &JsValue::from("host_call"), host_call_fn.as_ref().unchecked_ref())?;
-        // host_call_fn.forget();
-
-        
-
-        // #[derive(Clone)]
-        // struct Env {
-        //     // counter: Arc<Mutex<RefCell<u32>>>,
-        //     config: Arc<Mutex<RefCell<Config>>>,
-        // }
-        // let env = FunctionEnv::new(
-        //     &mut self.store,
-        //     Env {
-        //         // counter: shared_counter.clone(),
-        //         config: shared_counter.clone(),
-        //     },
-        // );
-        
         #[derive(Clone)]
         struct Env {
             exports: Arc<Mutex<RefCell<Option<Exports>>>>,
@@ -709,120 +591,4 @@ impl Blockless {
     pub fn set_stdin_string(&mut self, input: String) -> Result<(), JsValue> {
         self.set_stdin_buffer(input.as_bytes())
     }
-
-    // #[wasm_bindgen(js_name = hostExports)]
-    // pub fn host_exports(&self) -> Result<js_sys::Object, JsValue> {
-    //     let blockless_exports: JsValue = Object::new().into();
-
-    //     let instance_arc = self.config.clone();
-    //     let log_fn = Closure::wrap(Box::new(move |ptr: u32, len: u32| {
-    //         let Some(ref instance) = (*instance_arc.borrow()).instance else {
-    //             console_error!("Guest instance should have been set");
-    //             return;
-    //         };
-    //         host_log(instance, ptr, len)
-    //     }) as Box<dyn FnMut(u32, u32)>);
-
-    //     let instance_arc = self.config.clone();
-    //     let host_call_fn = Closure::wrap(Box::new(move |ptr: u32, len: u32| {
-    //         let Some(ref _instance) = (*instance_arc.borrow()).instance else {
-    //             console_error!("Guest instance should have been set");
-    //             return 0;
-    //         };
-
-    //         host_call(instance_arc.clone(), ptr, len)
-    //     }) as Box<dyn FnMut(u32, u32) -> u32>);
-
-    //     js_sys::Reflect::set(&blockless_exports, &JsValue::from("host_log"), log_fn.as_ref().unchecked_ref())?;
-    //     log_fn.forget();
-
-    //     js_sys::Reflect::set(&blockless_exports, &JsValue::from("host_call"), host_call_fn.as_ref().unchecked_ref())?;
-    //     host_call_fn.forget();
-        
-    //     let map = Map::new();
-    //     map.set(&JsValue::from("blockless"), &blockless_exports);
-    //     Ok(Object::from_entries(&map.into())?)
-    // }
-
 }
-
-use bls_common::{types::{ModuleCall, ModuleCallResponse}, http::HttpResponse};
-
-// pub fn host_log(instance: &WebAssembly::Instance, ptr: u32, len: u32) {
-//     let data = utils::decode_data_from_memory(&instance, ptr, len);
-//     let msg = std::str::from_utf8(&data).unwrap();
-
-//     console_log!("host_log: {}", msg);
-// }
-
-// pub fn host_call(config: Arc<RefCell<Config>>, ptr: u32, len: u32) -> u32 { // Allocate space in the guest's memory to store the return string
-//     // get the instance again - using interior mutability
-//     let cfg = config.borrow();
-//     let instance = cfg.instance.as_ref().expect("Guest instance should have been set");
-//     let permissions = &cfg.permissions;
-
-//     let call_data = utils::decode_data_from_memory(&instance, ptr, len);
-//     // console_log!("host_call: {}", std::str::from_utf8(&call_data).unwrap());
-
-//     // deserialize this to a canonical format
-//     let call = serde_json::from_slice::<ModuleCall>(&call_data).unwrap();
-//     call.validate_permissions(); // TODO: pass in config
-//     match call {
-//         ModuleCall::Http(http_req) => {
-//             console_log!("host_call: http_request called: {}", http_req);
-
-//             let blockless_callback = Reflect::get(&instance.exports(), &"blockless_callback".into())
-//                 .expect("callback export wasn't found")
-//                 .dyn_into::<js_sys::Function>()
-//                 .expect("The blockless_callback function is not present");
-
-//             if !http_req.valid_permissions(permissions) {
-//                 let result_bytes = serde_json::to_vec(&ModuleCallResponse::Http(Err("invalid permissions".into())))
-//                     .expect("failed to serialize module call response");
-//                 let result_ptr = utils::encode_data_to_memory(instance, &result_bytes);
-//                 return result_ptr;
-//             }
-
-//             let cfg_ptr = config.clone();
-//             // wasm_bindgen_futures::spawn_local(async move {
-//             //     // get the instance again - using interior mutability
-//             //     let cfg = cfg_ptr.borrow();
-//             //     let instance = cfg.instance.as_ref().expect("Guest instance should have been set");
-
-//             //     let module_call_response = match http_req.request().await {
-//             //         Ok(response) => {
-//             //             match HttpResponse::from_reqwest(response).await {
-//             //                 Ok(res) => ModuleCallResponse::Http(Ok(res)),
-//             //                 Err(_) => ModuleCallResponse::Http(Err("failed to parse response".into())),
-//             //             }
-//             //         },
-//             //         Err(err) => {
-//             //             console_error!("Error while running start function: {}", err);
-//             //             ModuleCallResponse::Http(Err(err.to_string()))
-//             //         }
-//             //     };
-//             //     let result_bytes = serde_json::to_vec(&module_call_response).expect("failed to serialize module call response");
-//             //     let result_ptr = utils::encode_data_to_memory(instance, &result_bytes);
-
-//             //     // TODO: fix error being thrown on callback
-//             //     match blockless_callback.call1(&JsValue::undefined(), &JsValue::from(result_ptr)) {
-//             //         Ok(_) => console_log!("blockless_callback called successfully"),
-//             //         Err(err) => console_error!("Error while running blockless_callback {}", err.as_string().unwrap_or_default()),
-//             //     };
-//             // });
-//         },
-//         ModuleCall::Ipfs(ipfs_get) => {
-//             console_log!("host_call: ipfs_get called: {}", ipfs_get);
-//             // TODO: validate guest has exported function to callback into
-//             // TODO: perform the request in spawn_local
-//             // TODO: callback into guest with the result (in spawn_local)
-//         },
-//     };
-
-//     // TODO: serialize a canonical response format to end back to client
-//     let return_str = "<hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host> <hello world from host>";
-//     let return_bytes = return_str.as_bytes();
-
-//     let result_ptr = utils::encode_data_to_memory(&instance, return_bytes);
-//     result_ptr
-// }
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,6 @@
 // load host wasm module (isomorphic bls runtime/extensions)
 import init, { Blockless } from "../bls-runtime-wasm/pkg";
-import WASI from "wasi-js";
-import browserBindings from "wasi-js/dist/bindings/browser";
-
 await init();
-
-// setup browser based WASI
-import { fs } from 'memfs';
-// override default `writeSync` behaviour to parse html and append to document
-(fs as any).writeSync = (fd, buffer, offset, length, position, callback) => {
-    const string = new TextDecoder().decode(buffer);
-    writeOutputToDOM(string);
-    if (callback) {
-        callback(null, length);
-    }
-};
-const wasi = new WASI({
-    args: ["--my-arg"],
-    env: {
-        FOO: "FOO",
-        BAR: "BAR",
-        BLS_REQUEST_METHOD: "GET",
-        BLS_REQUEST_PATH: "/",
-        BLS_REQUEST_QUERY: "",
-    },
-    bindings: { ...browserBindings, fs },
-});
 
 const bls = new Blockless({
     env: {
@@ -46,82 +21,83 @@ const bls = new Blockless({
         "http://httpbin.org/anything",
         "file://a.go"
     ],
-    fs,
+    // fs,
 });
 
 // const wasmPath = "../simple.wasm";
 // const wasmPath = "../basics.wasm";
 // const wasmPath = "../release.wasm";
-// const wasmPath = "../target/wasm32-unknown-unknown/release/rust_sdk.wasm";
+// const wasmPath = "../crates/rust-sdk/target/wasm32-unknown-unknown/release/rust_sdk.wasm";
 const wasmPath = "../crates/rust-sdk/target/wasm32-wasi/release/rust_sdk.wasm";
 const wasmModule = await WebAssembly.compileStreaming(fetch(wasmPath));
+const _wasmInstance = bls.instantiate(wasmModule, {});
+const exitCode = bls.start();
+const stdout = bls.getStdoutString();
 
-const wasiImports = wasi.getImports(wasmModule);
+console.info(`${stdout}\n(exit code: ${exitCode})`);
+writeOutputToDOM(stdout);
 
-const imports = {
-    ...wasiImports,
-    // wasi_snapshot_preview1: wasi.wasiImport,
-    // wasi_snapshot_preview1: {
-    //     ...wasiImports.wasi_snapshot_preview1,
-    //     // environ_sizes_get(){ return 0; },
-    //     // environ_get() { return 0; },
-    //     // proc_exit() { return 0; },
-    //     // path_open() { return 0; },
-    //     // fd_close() { return 0; },
-    //     // fd_prestat_dir_name() { return 0; },
-    //     // fd_prestat_get() { return 0; },
-    //     // fd_write(fd, iovsPtr, iovsLength, bytesWrittenPtr){
-    //     //     const iovs = new Uint32Array(instance.exports.memory.buffer, iovsPtr, iovsLength * 2);
-    //     //     if(fd === 1) { //stdout
-    //     //         let text = "";
-    //     //         let totalBytesWritten = 0;
-    //     //         const decoder = new TextDecoder();
-    //     //         for(let i =0; i < iovsLength * 2; i += 2){
-    //     //             const offset = iovs[i];
-    //     //             const length = iovs[i+1];
-    //     //             const textChunk = decoder.decode(new Int8Array(instance.exports.memory.buffer, offset, length));
-    //     //             text += textChunk;
-    //     //             totalBytesWritten += length;
-    //     //         }
-    //     //         const dataView = new DataView(instance.exports.memory.buffer);
-    //     //         dataView.setInt32(bytesWrittenPtr, totalBytesWritten, true);
-    //     //         console.log(text);
-    //     //     }
-    //     //     return 0;
-    //     // },
-    // },
-    browser: {
-        run_reqwest: (ptr: number, len: number) => {
-            const instance = (globalThis as any).instance; // global variable value set by bls.instantiate
-            console.info("run_reqwest from browser import");
-            const memory = new Uint8Array((instance.exports.memory as any).buffer, ptr, len);
-            const url = new TextDecoder().decode(memory);
-            console.info(url);
-        },
-    },
-};
+function writeOutputToDOM(out: string) {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(out, "text/html");
+    const parsedHead = htmlDoc.head; // get the <head> element from the parsed document
+    const mainDocumentHead = document.head; // get the main document's <head> element
+
+    // move all children from parsedHead to mainDocumentHead
+    while (parsedHead.firstChild) {
+        mainDocumentHead.appendChild(parsedHead.firstChild);
+    }
+
+    const parsedBody = htmlDoc.body; // get the <body> element from the parsed document
+    const mainDocumentBody = document.body; // get the main document's <body> element
+
+    // move all children from parsedBody to mainDocumentBody
+    while (parsedBody.firstChild) {
+        mainDocumentBody.appendChild(parsedBody.firstChild);
+    }
+}
+
+// import WASI from "wasi-js";
+// import browserBindings from "wasi-js/dist/bindings/browser";
+// // setup browser based WASI
+// import { fs } from 'memfs';
+// // override default `writeSync` behaviour to parse html and append to document
+// (fs as any).writeSync = (fd, buffer, offset, length, position, callback) => {
+//     const string = new TextDecoder().decode(buffer);
+//     writeOutputToDOM(string);
+//     if (callback) {
+//         callback(null, length);
+//     }
+// };
+// const wasi = new WASI({
+//     args: ["--my-arg"],
+//     env: {
+//         FOO: "FOO",
+//         BAR: "BAR",
+//         BLS_REQUEST_METHOD: "GET",
+//         BLS_REQUEST_PATH: "/",
+//         BLS_REQUEST_QUERY: "",
+//     },
+//     bindings: { ...browserBindings, fs },
+// });
+// const wasiImports = wasi.getImports(wasmModule);
+// const imports = {
+//     ...wasiImports,
+//     // wasi_snapshot_preview1: wasi.wasiImport,
+//     browser: {
+//         run_reqwest: (ptr: number, len: number) => {
+//             const instance = (globalThis as any).instance; // global variable value set by bls.instantiate
+//             console.info("run_reqwest from browser import");
+//             const memory = new Uint8Array((instance.exports.memory as any).buffer, ptr, len);
+//             const url = new TextDecoder().decode(memory);
+//             console.info(url);
+//         },
+//     },
+// };
 // const { instance: inst, module } = await WebAssembly.instantiateStreaming(fetch(wasmPath), imports);
-
 // const wasmInstance = await WebAssembly.instantiate(wasmModule, imports);
 // (wasmInstance.exports as any)._start();
 
-// const wasmInstance = await WebAssembly.instantiate(wasmModule, imports);
-const wasmInstance = bls.instantiate(wasmModule, {});
-// wasi.setMemory(wasmInstance.exports.memory as any); // set memory for wasi
-
-// // wasi.start(wasmInstance);
-const exitCode = bls.start();
-const stdout = bls.getStdoutString();
-console.log(`${stdout}\n(exit code: ${exitCode})`);
-writeOutputToDOM(stdout);
-
-// console.log("running!");
-
-// const wasmInstance = bls.instantiate(wasmModule, {});
-// const wasmerExitCode = bls.start();
-// const stdout = bls.getStdoutString();
-// console.log(`${stdout}\n(exit code: ${wasmerExitCode})`);
-// writeOutputToDOM(stdout);
 
 // import { init as initWasmer, WASI as WASIWasmer } from '@wasmer/wasi';
 // import initWasmer, { WASI as WASIWasmer } from '../../../wasmer-js/pkg';
@@ -143,11 +119,7 @@ writeOutputToDOM(stdout);
 // writeOutputToDOM(stdout);
 
 
-
-
-
 // import initWasmer, { initSync, run, Runtime } from '../../../wasmer-js/pkg';
-
 // await initWasmer();
 // const runtime = new Runtime(1);
 // const instance = run(wasmModule, runtime, {
@@ -160,59 +132,3 @@ writeOutputToDOM(stdout);
 //     // args: ["--my-arg"],
 // });
 // const output = await instance.wait();
-
-
-// Read a string from the instance's memory.
-// function readString(ptr: number, len: number, instance: WebAssembly.Instance) {
-//     const m = new Uint8Array((instance.exports.memory as any).buffer, ptr, len);
-//     const decoder = new TextDecoder("utf-8");
-//     // return a slice of size `len` from the module's
-//     // memory, starting at offset `ptr`
-//     return decoder.decode(m.slice(0, len));
-// }
-
-// function deallocGuestMemory(ptr: number, len: number, instance: WebAssembly.Instance) {
-//     // call the module's `dealloc` function
-//     (instance.exports as any).dealloc(ptr, len);
-// }
-
-// // Copy `data` into the `instance` exported memory buffer.
-// function copyMemory(instance: WebAssembly.Instance, data: Uint8Array) {
-//     // the `alloc` function returns an offset in
-//     // the module's memory to the start of the block
-//     const ptr = (instance.exports as any).alloc(data.length);
-
-//     // create a typed `ArrayBuffer` at `ptr` of proper size
-//     const mem = new Uint8Array((instance.exports.memory as any).buffer, ptr, data.length);
-//     // copy the content of `data` into the memory buffer
-//     mem.set(new Uint8Array(data));
-//     // return the pointer
-//     return ptr;
-// }
-
-function writeOutputToDOM(out: string) {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(out, "text/html");
-
-    // Get the <head> element from the parsed document
-    const parsedHead = htmlDoc.head;
-
-    // Get the main document's <head> element
-    const mainDocumentHead = document.head;
-
-    // Move all children from parsedHead to mainDocumentHead
-    while (parsedHead.firstChild) {
-        mainDocumentHead.appendChild(parsedHead.firstChild);
-    }
-
-    // Get the <body> element from the parsed document
-    const parsedBody = htmlDoc.body;
-
-    // Get the main document's <body> element
-    const mainDocumentBody = document.body;
-
-    // Move all children from parsedBody to mainDocumentBody
-    while (parsedBody.firstChild) {
-        mainDocumentBody.appendChild(parsedBody.firstChild);
-    }
-}
