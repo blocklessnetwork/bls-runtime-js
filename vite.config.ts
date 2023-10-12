ef56e61fddb27dc64a977aa5511444396965e642
zees-dev <dev.786zshan@gmail.com>
2023-10-13 10:51:51 +1300

    added instructions for running docker IPFS local node for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
+// run local IPFS node (docker) with CORS enabled
+// docker run --rm -it --name ipfs-node -p 4001:4001 -p 5001:5001 --entrypoint sh ipfs/go-ipfs:latest
+// /usr/local/bin/start_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+// /usr/local/bin/start_ipfs daemon --migrate=true --agent-version-suffix=docker 
+
 export default defineConfig({
   server: {
     port: 8080,
